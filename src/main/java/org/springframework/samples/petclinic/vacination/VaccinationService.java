0aa57d69451ad9aa730ea3cc1951f917d6a1d093
Manuel Padilla Tabuenca <mpadillatabuenca@gmail.com>
2022-01-14 13:39:20 +0100

    Control laboratorio

--- src/main/java/org/springframework/samples/petclinic/vacination/VaccinationService.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/VaccinationService.java
@@ -1,22 +1,48 @@
 package org.springframework.samples.petclinic.vacination;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.samples.petclinic.pet.exceptions.DuplicatedPetNameException;
+import org.springframework.samples.petclinic.user.AuthoritiesRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
+@Service
 public class VaccinationService {
-    public List<Vaccination> getAll(){
-        return null;
+
+    private VaccinationRepository vaccinationRepository;
+
+    @Autowired
+    public VaccinationService(VaccinationRepository vaccinationRepository){
+        this.vaccinationRepository = vaccinationRepository;
     }
 
+    @Transactional
+    public List<Vaccination> getAll(){
+        return vaccinationRepository.findAll();
+    }
+
+    @Transactional
     public List<Vaccine> getAllVaccines(){
-        return null;
+        return vaccinationRepository.findAllVaccines();
     }
 
     public Vaccine getVaccine(String typeName) {
-        return null;
+        return vaccinationRepository.findVaccineByName(typeName);
     }
 
+    @Transactional(rollbackFor = UnfeasibleVaccinationException.class)
     public Vaccination save(Vaccination p) throws UnfeasibleVaccinationException {
-        return null;       
+        Pet pet = p.getVaccinatedPet();
+        PetType petType = pet.getType();
+        if(!petType.equals(p.getVaccine().getPetType())){
+            throw new UnfeasibleVaccinationException();
+        }
+
+        return vaccinationRepository.save(p);
     }
 
     
--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
@@ -1,15 +1,33 @@
 package org.springframework.samples.petclinic.vacination;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.model.NamedEntity;
+import org.springframework.samples.petclinic.pet.Pet;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
-public class Vaccine {
-    Integer id;
+@Entity
+public class Vaccine extends BaseEntity {
+
+    @Size(min = 3, max = 50)
+    @Column(unique = true)
     String name;
+
+    @DecimalMin("0.0")
     Double price;
-    PetType petType;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "pet_type_id")
+    private PetType petType;
 }
